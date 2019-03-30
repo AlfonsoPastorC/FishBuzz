f6553763badae092f001d39c657eadf223256135
Alfonso Pastor <a20165834@pucp.edu.pe>
2019-03-30 11:19:51 -0500

    posible version

--- Main.cpp
+++ Main.cpp
@@ -2,19 +2,28 @@
 using namespace std;
 
 int main() {
-	int i, j = 1, k = 1;
+	int i, j = 1, k = 1, m = 0, n = 0 , ñ = 0;
+	int fizz[100];
+	int buzz[100];
+	int fizzbuzz[100];
 
 	for (i = 1; i <= 100; i++) {
 		if ((j == 3) && (k == 5)) {
 			cout << "Fizzbuzz"<< endl;
 			j = 1;
 			k = 1;
+			fizz[m] = i;
+			m++;
 		}else if (j == 3){
 			j = 1;
 			cout << "Fizz" << endl;
+			buzz[n] = i;
+			n++;
 		}else if (k == 5) {
 			k = 1;
 			cout << "Buzz" << endl;
+			fizzbuzz[ñ] = i;
+			ñ++;
 		}
 		else {
 			cout << i << endl;
